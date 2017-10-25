5965cb6701c55cc881878ac08201e97128d1e183
guanzhou.song@gmail.com <Song.85238799>
2017-10-25 21:28:55 +0800

    up

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -7,7 +7,7 @@ function idx = findClosestCentroids(X, centroids)
 
 % Set K
 K = size(centroids, 1);
-
+data = zeros(size(X,1),1);
 % You need to return the following variables correctly.
 idx = zeros(size(X,1), 1);
 
@@ -20,8 +20,14 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
+    
+for i=1:size(X,1)
+    temp=zeros(K,1);
+    for j=1:K
+        temp(j)=findDistance(X(i,:),centroids(j,:));
+    end
+    [data,idx(i)]= min(temp(:));
+end
 
 
 
---
+++ ex7/findDistance.m
@@ -0,0 +1,3 @@
+function dis = findDistance(x,y)
+        dis = sum((x-y).^2).^(0.5);
+    end
